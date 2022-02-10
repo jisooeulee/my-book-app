2763c2af8c2aa250a8df023197743d9a389f48c7
jisooeulee <traveler279c@gmail.com>
2022-02-10 19:56:49 +0900

    [modify] change writeReadBookInfo method, create fetchBookInfo method

--- MyBookApp/Database/FirebaseProcessor.swift
+++ MyBookApp/Database/FirebaseProcessor.swift
@@ -17,9 +17,11 @@ class FirebaseProcessor {
     private init() {}
     
     /// 本の情報をDBに保存する
-    func writeReadBookInfo(id: String) {
+    func writeReadBookInfo(id: String, imageUrl: String, title: String, authors: String, description: String) {
         let boookItemRef = bookRef.child(id)
-        let itemValues: [String: Any] = ["isRead": true]
+        let itemValues: [String: Any] = [
+            "isRead": true, "id": id, "imageUrl": imageUrl, "title": title, "authors": authors, "description": description,
+        ]
         
         boookItemRef.setValue(itemValues)
     }
@@ -35,6 +37,32 @@ class FirebaseProcessor {
                 isRead = false
                 completion(isRead)
             }
+        }
+    }
+    
+    /// 読んだ本のリストを取得する
+    func fetchBookInfo(completion: @escaping (_ result: [Item]) -> Void) {
+        var items: [Item] = []
+        
+        bookRef.observeSingleEvent(of: .value) { snapshot in
+            guard let value = snapshot.value as? [String: AnyObject] else {
+                completion(items)
+                return
+            }
+            
+            for volumeInfo in value.values {
+                let id = volumeInfo["id"] as! String
+                let imageUrl = volumeInfo["imageUrl"] as! String
+                let title = volumeInfo["title"] as! String
+                let authors = volumeInfo["authors"] as! String
+                let description = volumeInfo["description"] as! String
+                
+                let volumesInfo = VolumeInfo(title: title, authors: [authors], publishedDate: nil, imageLinks: ImageLinks(smallThumbnail: nil, thumbnail: imageUrl), description: description, googleBooksWebLink: nil, categories: nil, publisher: nil, pageCount: nil)
+                
+                let item = Item(id: id, volumeInfo: volumesInfo, saleInfo: nil)
+                items.append(item)
+            }
+            completion(items)
         }
     }
     
