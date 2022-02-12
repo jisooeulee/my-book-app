bf427312eb5240081d77daac207ab586fd38ea6b
jisooeulee <traveler279c@gmail.com>
2022-02-12 19:43:59 +0900

    [modify] change parameter name

--- MyBookApp/Scene/DetailScene/DetailView.swift
+++ MyBookApp/Scene/DetailScene/DetailView.swift
@@ -15,7 +15,7 @@ class DetailView: UIView {
         didSet {
             guard let item = self.item else { return }
             
-            FirebaseProcessor.shared.searchBookInfo(keyword: item.id) { result in
+            FirebaseProcessor.shared.searchBookInfo(item.id) { result in
                 ReadStatus.shared.isRead = result
                 
                 if ReadStatus.shared.isRead {
